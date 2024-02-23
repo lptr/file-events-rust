f29257d2c3fe991eb9ffa957f964db8f60ca7a80
Lóránt Pintér <lorant@gradle.com>
2024-02-23 11:33:58 +0100

    Run Cargo from the Gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,36 @@ plugins {
 application {
     mainClass = "org.gradle.test.Main"
 }
+
+@CacheableTask
+abstract class CarguBuild : DefaultTask() {
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.NONE)
+    abstract val sources: ConfigurableFileCollection
+
+    @get:OutputDirectory
+    abstract val destinationDirectory: DirectoryProperty
+
+    @Inject
+    abstract fun getExecOperations(): ExecOperations
+
+    @TaskAction
+    fun taskAction() {
+        getExecOperations().exec {
+            commandLine(
+                "cargo", "build",
+                "--config", "build.target-dir = \"" + destinationDirectory.get().asFile.absolutePath + "\"",
+            )
+        }
+    }
+}
+
+val buildRustLib by tasks.registering(CarguBuild::class) {
+    // This is just an assumption about what files we are going to consume
+    sources.from(project.layout.files("Cargo.toml", "Cargo.lock", "src/main/rust"))
+    destinationDirectory = project.layout.buildDirectory.dir("rust")
+}
+
+tasks.named<ProcessResources>("processResources") {
+    from(buildRustLib.flatMap { it.destinationDirectory.file("debug/libfile_events.dylib") })
+}
--- src/main/java/org/gradle/test/Main.java
+++ src/main/java/org/gradle/test/Main.java
@@ -1,9 +1,30 @@
 package org.gradle.test;
 
-public class Main {
-    public static void main(String[] args) {
-        System.load(System.getProperty("user.dir") + "/build/cargo/debug/libfile_events.dylib");
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Path libFile = Files.createTempFile("libfile_events", ".dylib");
+        try (InputStream resourceStream = Main.class.getClassLoader().getResourceAsStream("libfile_events.dylib")) {
+            if (resourceStream == null) {
+                throw new IllegalArgumentException("Cannot find library");
+            }
+            Files.copy(resourceStream, libFile, StandardCopyOption.REPLACE_EXISTING);
+        }
+        try {
+            System.load(libFile.toAbsolutePath().toString());
+
+            runWithNativeLibraryLoaded();
+        } finally {
+            Files.delete(libFile);
+        }
+    }
+
+    private static void runWithNativeLibraryLoaded() {
         String output = FileEvents.hello("Dezső");
         System.out.println(output);
     }
